8cd5043255e2d56318f1641c59a33482588d2c2f
hyust <hyust@DESKTOP-BIFTPAD>
2025-05-27 21:13:20 +0300

    write\read txt

--- Taxes base.cpp
+++ Taxes base.cpp
@@ -3,3 +3,21 @@
 #include <string>
 
 
+
+
+void create_plist(int logpass) {
+std::string text;
+std::ifstream names("names_sub.txt");
+while (getline(names, text)) {
+		std::cout << text;
+	}
+
+}
+
+int main() {
+	std::ofstream ipass("ipass.txt");
+	ipass << 1 << std::endl;
+	ipass.close();
+	int logs = 0;
+	create_plist(logs);
+}
