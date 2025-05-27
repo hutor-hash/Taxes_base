19c9083f6f7d1d4c9b8b4d2cab46d548c095f08d
hyust <hyust@DESKTOP-BIFTPAD>
2025-05-27 22:51:14 +0300

    started doing polymorfism

--- Taxes base.cpp
+++ Taxes base.cpp
@@ -3,35 +3,29 @@
 #include <string>
 #include <vector>
 
-void create_plist(int* logpass) {
-std::string text;
-std::ifstream names("names_sub.txt");
-char lettersb[52] = { 'q','w','e','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M' };
-srand(time(0));
-int rando = rand() % 20;
-std::cout << rando;
-for (int i = 0; i < 21; ++i) {
-	getline(names, text);
-	if (i == rando) {
-		std::cout << text;
-	}
-	for (int i = 0; i < text.length(); ++i) {
-		int zerc = text.length() - 11;
+class User{
+public:
+	int pasword_check(int login, int passwd) {
+		int check = 0;
+		std::ifstream ipass("ipass.txt");
+		int flogin, fpasswd; //сделай проверку пароллей через файл
 		
-		for (int i = 0; i < zerc; ++i)
-		{
-			logpass[i] = 0;
-			std::cout << logpass[i];
-		}
-	}
+		if (login == flogin)
+			check++;
+		if (passwd == fpasswd)
+			check++;
+		if (check == 2)
+			return 1;
+		return 0;
+}
 }
 
-}
+class tax_payer : public User {
+public:
+	int 
+};
 
 int main() {
-	std::ofstream ipass("ipass.txt");
-	ipass << 1 << std::endl;
-	ipass.close();
-	std::vector<int> logs(1);
-	create_plist(logs);
+
+	return 0;
 }
