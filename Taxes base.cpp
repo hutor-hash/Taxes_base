5bcdc3edab8a988f0462fa5be683b997407d375e
hyust <hyust@DESKTOP-BIFTPAD>
2025-05-27 21:09:46 +0300

    file reading\writing test

--- Taxes base.cpp
+++ Taxes base.cpp
@@ -1,10 +1,21 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
 
-class user {
-private:
-	std::string name;
-	std::string surname;
-	std::string income;
+void create_plist(int logpass) {
+	std::string text;
+	std::ifstream names("names_sub.txt");
+	while (getline(names, text)) {
+		std::cout << text;
+	}
 
-};
+}
+
+int main() {
+	std::ofstream ipass("ipass.txt");
+	ipass << 1 << std::endl;
+	ipass.close();
+	int logs=0;
+	create_plist(logs);
+}
 
