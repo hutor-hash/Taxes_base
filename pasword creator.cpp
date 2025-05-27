56478665e167ba3b5eca024724d126d062a60b21
hyust <hyust@DESKTOP-BIFTPAD>
2025-05-27 22:34:01 +0300

    didn't get luck in convertor...

---
+++ pasword creator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+void create_plist() {
+	std::string text;
+	std::vector<int> logs(1);
+	std::ifstream names("names_sub.txt");
+	char lettersb[52] = { 'q','w','e','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M' };
+	srand(time(0));
+	int rando = rand() % 20;
+	std::cout << rando;
+	for (int i = 0; i < 21; ++i) {
+		getline(names, text);
+		if (i == rando) {
+			std::cout << text;
+		}
+		for (int i = 0; i < text.length(); ++i) {
+			int zerc = text.length() - 11;
+
+			for (int i = 0; i < zerc; ++i)
+			{
+				logpass[i] = 0;
+				std::cout << logpass[i];
+			}
+		}
+	}
+
+}
