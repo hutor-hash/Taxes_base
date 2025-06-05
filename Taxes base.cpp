9c48ca752b54561bc47f05f291512eea0bd7fe3a
hyust <hyust@DESKTOP-BIFTPAD>
2025-06-05 19:51:20 +0300

    created more or less beta of working program but without polimorfism and data sets

--- Taxes base.cpp
+++ Taxes base.cpp
@@ -2,30 +2,48 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
-class User{
-public:
-	int pasword_check(int login, int passwd) {
-		int check = 0;
-		std::ifstream ipass("ipass.txt");
-		int flogin, fpasswd; //сделай проверку пароллей через файл
-		
-		if (login == flogin)
-			check++;
-		if (passwd == fpasswd)
-			check++;
-		if (check == 2)
-			return 1;
-		return 0;
-}
-}
-
-class tax_payer : public User {
-public:
-	int 
-};
+#include <stdlib.h>
 
 int main() {
-
-	return 0;
+	std::string prog = "1";
+	//std::ofstream logp("ipass.txt");
+	int logp = 001, pass = 231, attempts=3, info=34; //instead of numbers attach txt files
+	int login, paswd, acces = 0;
+	std::cout << "What do you want today?\nType login to acces you account and check taxes\nType exit to shut down program\n";
+	std::cin >> prog;                 //stting prog wiil be the main control word
+	if (prog == "login") {
+		std::cout << "Enter login: ";
+		std::cin >> login;
+		while (attempts > 0) {
+			if (logp == login) {                       //checks login
+				std::cout << "\nEnter password: ";
+				std::cin >> paswd;
+				if (pass == paswd) {                     //checks password
+					acces = 1;
+					attempts = -1;
+				}
+				else {
+					attempts = attempts - 1;
+					std::cout << "Incorect password.\nAttempts left: " << attempts << "\n";
+				}
+				if (attempts == -1) {
+					prog = "aproved";                     //assign to main control access state
+					//system("cls");
+				}
+			}
+			else
+			{
+				std::cout << "There is no such user in data base, or you make a mistake in login.\nTry again";
+				std::cin >> login;
+			}
+			//system("cls");
+		}
+	}
+	if (prog == "aproved") {
+		std::cout <<"Taxes needed to be payed: " << info<<"\nTo close the program type exit\n"; //attach the data bast .txt
+		std::cin >> prog;                                       //will acces your data base
+	}
+	if (prog == "exit" || prog == "e" || prog == "ex")                //just ends the program
+		return 0;
+return 0;
 }
