b4926eb0c2d8a39262ed8247cbbbce8b55c542c1
hyust <hyust@DESKTOP-BIFTPAD>
2025-06-07 18:02:20 +0300

    Created the file account creator, it's pieca of code creates everytime new account, soon will rewrite login part and add to it firstly random generating tax and then admin editing

--- Taxes base.cpp
+++ Taxes base.cpp
@@ -3,15 +3,22 @@
 #include <string>
 #include <vector>
 #include <stdlib.h>
+#include <cstring>
+#include <filesystem>
+
+struct account {
+	int log;
+	int password;
+};
 
 int main() {
 	std::string prog = "1";
-	std::ofstream logp1("ipass.txt");            //file reader
-	while(logp1)
+	//std::ofstream logp1("ipass.txt");            //file reader
 	int logp = 001, pass = 231, attempts=3, info=34; //instead of numbers attach txt files
 	int login, paswd, acces = 0;
-	std::cout << "What do you want today?\nType login to acces you account and check taxes\nType exit to shut down program\n";
+	std::cout << "What do you want today?\nType login to acces you account and check taxes\nType exit to shut down program\nType create to create an account\n";
 	std::cin >> prog;                 //stting prog wiil be the main control word
+	system("cls");
 	if (prog == "login") {
 		std::cout << "Enter login: ";
 		std::cin >> login;
@@ -29,7 +36,7 @@ int main() {
 				}
 				if (attempts == -1) {
 					prog = "aproved";                     //assign to main control access state
-					//system("cls");
+					system("cls");
 				}
 			}
 			else
@@ -37,14 +44,65 @@ int main() {
 				std::cout << "There is no such user in data base, or you make a mistake in login.\nTry again\n";
 				std::cin >> login;
 			}
-			//system("cls");
+			system("cls");
 		}
 	}
+
+	else if (prog == "exit" || prog == "e" || prog == "ex") {                //just ends the program
+		return 0;
+	}
+	else if (prog == "create" || prog == "Create" || prog == "creat" || prog == "c") {
+		account a;
+		std::cout << "enter your new login: ";
+		std::cin >> a.log;
+		std::cout << "\nEnter your new password: ";
+		std::cin >> a.password;
+
+		std::ifstream counter("accs/counter.txt");
+		int counte;
+		counter >> counte;
+		counte = counte + 1;										//+1 user everytime
+		counter.close();
+		remove("accs/counter.txt");									//removes old file
+		std::ifstream check("accs/counter.txt");
+		if (!check) {
+			std::cout << "\nThe counter has been updated!\n";       //checks if file reomoved
+		}
+		else {
+			std::cout << "\nSomething went wrong";
+		};
+
+		std::ofstream counter1("accs/counter.txt", std::ios::trunc); //and creates new one with updated user count
+		counter1 << counte;
+		counter1.close();
+
+		std::cout << "you are " << counte << " user in system!";
+		std::string mainname = "user.bin";
+		std::string way = "accs/";                                   //necesary extensions for file creation
+		std::string filename = way + std::to_string(counte) + mainname; //constructing dile name
+		std::cout << "\nfilename: " << filename;
+		std::ofstream outfile(filename, std::ios::binary);           //creates bin file
+
+		if (!outfile)
+			std::cout << "cannot opne file\n";
+		outfile.write(reinterpret_cast<char*>(&a), sizeof(account));
+		outfile.close();
+		std::ifstream itfile(filename, std::ios::binary);
+		itfile.read(reinterpret_cast<char*>(&a), sizeof(account));   //I've created it just for test of reading bin files
+		outfile.close();
+
+		std::cout << "\nYour log: " << a.log << "\nYour password: " << a.password;
+
+	}
+	else {
+		std::cout << "there is no such command\n";
+	};
+	
+
 	if (prog == "aproved") {
-		std::cout <<"Taxes needed to be payed: " << info<<"\nTo close the program type exit\n"; //attach the data bast .txt
+		std::cout << "Taxes needed to be payed: " << info << "\nTo close the program type exit\n"; //attach the data bast .txt
 		std::cin >> prog;                                       //will acces your data base
 	}
-	if (prog == "exit" || prog == "e" || prog == "ex")                //just ends the program
-		return 0;
 return 0;
 }
+
